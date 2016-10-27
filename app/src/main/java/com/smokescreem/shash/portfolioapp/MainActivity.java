dc38d197eb23a017f792d88973bad89c2d863416
smoke572 <shashwata275@gmail.com>
2016-10-27 20:11:50 +0530

    My first commit

---
+++ app/src/main/java/com/smokescreem/shash/portfolioapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.smokescreem.shash.portfolioapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
